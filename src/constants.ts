655784d97c49289b3821e1fcfa48da6763b6259f
roecoinsol <roecoinsol@gmail.com>
2025-01-11 23:17:14 +0000

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { PublicKey } from '@solana/web3.js'
 import { FAKE_TOKEN_MINT, PoolToken, TokenMeta, makeHeliusTokenFetcher } from 'gamba-react-ui-v2'
 
 // Get RPC from the .env file or default to the public RPC.
-export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? 'https://cold-hanni-fast-mainnet.helius-rpc.com/'
+export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? 'https://mainnet.helius-rpc.com/?api-key=62362705-e7eb-4a76-9a82-df7e329e2d40'
 
 // Solana address that will receive fees when somebody plays on this platform
 export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
